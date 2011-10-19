1c7e63903cbec4da02130cf2e0bd2fc2da767a7b
Sinoru <sinoru.dev@me.com>
2011-10-20 00:16:59 +0900

    Initial Commit

---
+++ Reachability/Reachability.h
@@ -0,0 +1,13 @@
+//
+//  Reachability.h
+//  Reachability
+//
+//  Created by 재홍 강 on 11. 10. 20..
+//  Copyright (c) 2011년 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Reachability : NSObject
+
+@end
---
+++ Reachability/Reachability.m
@@ -0,0 +1,23 @@
+//
+//  Reachability.m
+//  Reachability
+//
+//  Created by 재홍 강 on 11. 10. 20..
+//  Copyright (c) 2011년 __MyCompanyName__. All rights reserved.
+//
+
+#import "Reachability.h"
+
+@implementation Reachability
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
